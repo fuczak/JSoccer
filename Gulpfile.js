977e310607c55b823160d217bb91348484ade77c
Damian Prus <prus.damian@gmail.com>
2015-08-03 20:33:31 +0200

    Initialize repository with gulp set up

---
+++ Gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    useref = require('gulp-useref'),
+    browserify = require('browserify'),
+    watchify = require('watchify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    gutil = require('gulp-util'),
+    sourcemaps = require('gulp-sourcemaps'),
+    assign = require('lodash.assign'),
+    browserSync = require('browser-sync');
+
+var customOpts = {
+  entries: ['./src/scripts/main.js'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+gulp.task('sass', function () {
+  gulp.src('./src/styles/**/*.scss')
+  .pipe(sass({
+    outputStyle: 'compressed',
+    includePaths: './bower_components/bootstrap-sass/assets/stylesheets'
+  }).on('error', sass.logError))
+  .pipe(gulp.dest('./dist/styles'))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('html', function () {
+  gulp.src('./src/*.html')
+  .pipe(useref())
+  .pipe(gulp.dest('./dist'))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browserify', function () {
+  b.bundle()
+  .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+  .pipe(source('bundle.js'))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({loadMaps: true}))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./dist/scripts'))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('serve', function() {
+  browserSync({
+    server: './dist'
+  });
+  gulp.watch('./src/styles/**/*.scss', ['sass']);
+  gulp.watch('./src/*.html', ['html']);
+  gulp.watch('./src/scripts/**/*.js', ['browserify']);
+});
