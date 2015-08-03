95699767cccfa61e252ed213fae142f787aa1566
Damian Prus <prus.damian@gmail.com>
2015-08-03 21:10:44 +0200

    Add teams module

--- src/scripts/intro.js
+++ src/scripts/intro.js
@@ -1,5 +1,11 @@
+var teams = require('./teams')
+
 function init() {
-  console.log('test');
+  var playerTeamSelect = document.getElementById('player-team-select');
+  var cpuTeamSelect = document.getElementById('cpu-team-select');
+
+  playerTeamSelect.options[0] = new Option('Text 1', 'value 1');
+
 }
 
 module.exports = init;
