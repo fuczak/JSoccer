e5dd48115b6cae724748fe491a32f5c022ff7d01
fuczak <prus.damian@gmail.com>
2015-08-04 11:49:29 +0200

    Add ramjet to initialize game with user chosen team

--- src/scripts/intro.js
+++ src/scripts/intro.js
@@ -1,11 +1,26 @@
-var teams = require('./teams')
+var teams = require('./teams');
+var select = require('./selectors');
+var ramjet = require('ramjet');
+
+function handler(e) {
+	if (e.target.classList.contains('flag')) {
+		select.userTeam.classList.remove('ramjet-hidden');
+		select.userTeam.innerHTML = e.target.innerHTML;
+		ramjet.transform(e.target, select.userTeam, {
+			done: function() {
+				select.userTeam.classList.remove('ramjet-hidden');
+				select.overlay.classList.add('overlay-hide');
+			},
+			duration: 250
+		});	
+		e.target.classList.add('ramjet-hidden');
+		select.userTeam.classList.add('ramjet-hidden');
+	}
+	select.overlay.removeEventListener('click', handler);
+}
 
 function init() {
-  var overlay = document.getElementsByClassName('overlay')[0];
-
-  overlay.addEventListener('click', function(e) {
-  	this.classList.toggle('overlay-hide');
-  });
+  select.overlay.addEventListener('click', handler);  
 }
 
 module.exports = init;
---
+++ src/scripts/selectors.js
@@ -0,0 +1,6 @@
+var selectors = {
+	overlay: document.getElementsByClassName('overlay')[0],
+	userTeam: document.getElementById('userTeam')
+}
+
+module.exports = selectors;
