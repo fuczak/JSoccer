3c220377e1005a48644eef93f35ebb7e682aba82
fuczak <prus.damian@gmail.com>
2015-08-05 13:38:18 +0200

    Add bootstrap js and layout for tactic setup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,11 @@
+// Workaround to make bootstrap js work correctly
+window.jQuery = require('jquery');
+require('bootstrap-sass');
+
 var intro = require('./intro.js');
 var select = require('./selectors');
-
 intro();
+
 
 // select.tacticSetup.addEventListener('click', function(e) {
 // 	this.classList.add('tactic-setup-hide');
