be2e7f7b180cef39e4a2f756dd3feae48718ec70
fuczak <prus.damian@gmail.com>
2015-08-05 13:14:53 +0200

    Start adding formation radio button group

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,6 @@ var select = require('./selectors');
 
 intro();
 
-select.tacticSetup.addEventListener('click', function(e) {
-	this.classList.add('tactic-setup-hide');
-});
+// select.tacticSetup.addEventListener('click', function(e) {
+// 	this.classList.add('tactic-setup-hide');
+// });
