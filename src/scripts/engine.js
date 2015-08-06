31737847425cc74b7a1e82ce1103a76f7ca939a1
Damian Prus <prus.damian@gmail.com>
2015-08-06 07:36:27 +0200

    Add easing to card to commentary transition

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,5 @@
 var $ = require('jquery');
+var ramjet = require('ramjet');
 
 var engine = {
   init: init
@@ -8,8 +9,21 @@ function init() {
   for (var i = 0; i < 24; i++) {
     $('#pitch').find('.row').append('<div class="col-xs-3 col-md-2 card"><img src="images/flags/Burundi.png"></div>');
   }
-  $('.card').on('click', function() {
+  $('.card').on('click', function(e) {
     $(this).css('background-color', 'red');
+    var a = e.target;
+    var b = document.getElementById('commentary');
+
+    b.classList.remove('ramjet-hidden');
+    ramjet.transform(a, b, {
+      done: function() {
+        b.classList.remove('ramjet-hidden');
+      },
+      duration: 800,
+      easing: ramjet.easeInOut
+    });
+    a.classList.add('ramjet-hidden');
+    b.classList.add('ramjet-hidden');
   });
 }
 
