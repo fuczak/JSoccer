8bac12c1797b8e29609c6bd75ff173a5d2f1d433
fuczak <prus.damian@gmail.com>
2015-08-06 11:48:00 +0200

    Add remaining cards sidebar

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -12,8 +12,10 @@ function random(min, max) {
 function arrangeButtons() {
 	if ($(window).innerWidth() > 992) {
 		$('.sidebar').find('div').removeClass('btn-group').addClass('btn-group-vertical');
+		$('.remaining').css('display', 'block');
 	} else {
 		$('.sidebar').find('div').removeClass('btn-group-vertical').addClass('btn-group');
+		$('.remaining').css('display', 'inline-block');
 	}
 }
 
