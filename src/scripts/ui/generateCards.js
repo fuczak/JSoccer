5915a7e380db3aaf85d4714e1b2e96dfd1bd2b66
Damian Prus <prus.damian@gmail.com>
2015-08-08 18:50:29 +0200

    Add IDs to generated cards

--- src/scripts/ui/generateCards.js
+++ src/scripts/ui/generateCards.js
@@ -2,6 +2,6 @@ var $ = require('jquery');
 
 module.exports = function() {
 	for (var i = 0; i < 24; i++) {
-	  $('#pitch').find('.row').append('<div class="col-xs-3 col-md-2 card"></div>');
+	  $('#pitch').find('.row').append('<div class="col-xs-3 col-md-2 card" id="' + i + '"></div>');
 	}
-}
+};
