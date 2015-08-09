a908dba6435bb3a3af41815369cab42733d2c246
Damian Prus <prus.damian@gmail.com>
2015-08-09 20:15:19 +0200

    Add get outcome method that can return outcome and its index for both player and cpu picks

--- src/scripts/outcomes.js
+++ src/scripts/outcomes.js
@@ -8,7 +8,7 @@ var _outcomeArray = [];
 var outcomes = {
   generate: generate,
   clear: clear,
-  getOutcome: getOutcome,
+  getOutcome: getOutcome
 };
 
 function generate() {
@@ -16,13 +16,13 @@ function generate() {
   var whistleEvents = helpers.random(1, 3);
   var outcomesForUi = [0, 0, 0, 0, 0];
   for (var i = 0; i < whistleEvents; i++) {
-    _outcomeArray.push('Whistle');
+    _outcomeArray.push({type: 'Whistle', picked: false});
     outcomesForUi[4] += 1;
   }
   // Fill the rest of the array with random events
   for (var j = 0; j < 24 - whistleEvents; j++) {
     var index = helpers.random(0, 7);
-    _outcomeArray.push(_possibleOutcomes[index]);
+    _outcomeArray.push({type: _possibleOutcomes[index], picked: false});
     index < 4 ? outcomesForUi[index] += 1 : outcomesForUi[4] += 1;
   }
   // Shuffle the array
@@ -36,7 +36,17 @@ function clear() {
 }
 
 function getOutcome(index) {
-  return _outcomeArray[index];
+  if (index === undefined) {
+    var pickedIndex = helpers.random(0, 23);
+    if (_outcomeArray[pickedIndex].picked) {
+      return getOutcome();
+    } else {
+      _outcomeArray[pickedIndex].picked = true;
+      return {outcome: _outcomeArray[pickedIndex], index: pickedIndex};
+    }
+  }
+  _outcomeArray[index].picked = true;
+  return {outcome: _outcomeArray[index], index: index};
 }
 
 module.exports = outcomes;
