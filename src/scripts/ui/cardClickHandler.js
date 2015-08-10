4f80826e813fa4930caa43a57cb6e459be2d70db
Damian Prus <prus.damian@gmail.com>
2015-08-10 21:07:43 +0200

    Add proper throttling of user actions (one every 1000ms)

--- src/scripts/ui/cardClickHandler.js
+++ src/scripts/ui/cardClickHandler.js
@@ -1,8 +1,16 @@
 var $ = require('jquery');
 var engine = require('../engine/index');
+var animating = false;
 
 module.exports = function(e) {
 	// Return if card has been already clicked
 	if ($(this).hasClass('ramjet-hidden')) return;
-	engine.handleCardClick(e);
+	// Respond to user clicks only after one second since last click
+	if (!animating) {
+		animating = true;
+		engine.handleCardClick(e);
+		setTimeout(function() {
+			animating = false;
+		}, 1000);
+	}
 };
--- src/scripts/ui/cardToCommentary.js
+++ src/scripts/ui/cardToCommentary.js
@@ -1,26 +1,23 @@
 var ramjet = require('ramjet');
-var isAnimating;
 
-module.exports = function(index, outcome) {	
+module.exports = function(index, outcome) {
 	var a = document.getElementById(index);
 	var b = document.getElementById('commentary');
-	if (!isAnimating) {
-		b.children[0].classList.remove('entered');
-		setTimeout(function() {
-			isAnimating = true;
-			b.children[0].textContent = '';	
-			b.classList.remove('ramjet-hidden');
-			ramjet.transform(a, b, {
-			  done: function() {
-			   b.children[0].textContent = outcome;	  
-			   b.children[0].classList.add('entered'); 
-			   b.classList.remove('ramjet-hidden');
-			   isAnimating = false;
-			  },
-			  duration: 600,
-			  easing: ramjet.easeInOut
-			});
-			a.classList.add('ramjet-hidden');	
-		}, 150);	
-	}
+	b.children[0].classList.remove('entered');
+	setTimeout(function() {
+		isAnimating = true;
+		b.children[0].textContent = '';
+		b.classList.remove('ramjet-hidden');
+		ramjet.transform(a, b, {
+		  done: function() {
+		   b.children[0].textContent = outcome;
+		   b.children[0].classList.add('entered');
+		   b.classList.remove('ramjet-hidden');
+		   isAnimating = false;
+		  },
+		  duration: 600,
+		  easing: ramjet.easeInOut
+		});
+		a.classList.add('ramjet-hidden');
+	}, 150);
 };
