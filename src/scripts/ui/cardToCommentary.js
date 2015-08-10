7872eb55a02fea4414355a56162845646aca7d83
fuczak <prus.damian@gmail.com>
2015-08-10 15:58:26 +0200

    Add nice animations to commentary box, but should find a way for better user input throttling

--- src/scripts/ui/cardToCommentary.js
+++ src/scripts/ui/cardToCommentary.js
@@ -5,16 +5,18 @@ module.exports = function(index, outcome) {
 	var b = document.getElementById('commentary');
 	
 	b.children[0].classList.remove('entered');
-	b.children[0].textContent = '';
-	b.classList.remove('ramjet-hidden');
-	ramjet.transform(a, b, {
-	  done: function() {
-	   b.children[0].textContent = outcome;	  
-	   b.children[0].classList.add('entered'); 
-	   b.classList.remove('ramjet-hidden');
-	  },
-	  duration: 600,
-	  easing: ramjet.easeInOut
-	});
-	a.classList.add('ramjet-hidden');	
+	setTimeout(function() {
+		b.children[0].textContent = '';	
+		b.classList.remove('ramjet-hidden');
+		ramjet.transform(a, b, {
+		  done: function() {
+		   b.children[0].textContent = outcome;	  
+		   b.children[0].classList.add('entered'); 
+		   b.classList.remove('ramjet-hidden');
+		  },
+		  duration: 600,
+		  easing: ramjet.easeInOut
+		});
+		a.classList.add('ramjet-hidden');	
+	}, 150);
 };
