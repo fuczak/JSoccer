8ccc680f3349490f1eb30ab3c3821f24cd7abe0b
fuczak <prus.damian@gmail.com>
2015-08-10 15:42:06 +0200

    Rearrange scripts for social buttons, add id to mentality buttons and modify the commentary painting function

--- src/scripts/ui/cardToCommentary.js
+++ src/scripts/ui/cardToCommentary.js
@@ -1,18 +1,20 @@
-var $ = require('jquery');
 var ramjet = require('ramjet');
 
 module.exports = function(index, outcome) {
 	var a = document.getElementById(index);
 	var b = document.getElementById('commentary');
-
+	
+	b.children[0].classList.remove('entered');
+	b.children[0].textContent = '';
 	b.classList.remove('ramjet-hidden');
 	ramjet.transform(a, b, {
 	  done: function() {
-	    b.classList.remove('ramjet-hidden');
-	    b.textContent = outcome;
+	   b.children[0].textContent = outcome;	  
+	   b.children[0].classList.add('entered'); 
+	   b.classList.remove('ramjet-hidden');
 	  },
 	  duration: 600,
 	  easing: ramjet.easeInOut
 	});
-	a.classList.add('ramjet-hidden');
+	a.classList.add('ramjet-hidden');	
 };
