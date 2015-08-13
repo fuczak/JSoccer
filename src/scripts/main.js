b1007384b3aebb1b67ff7e8cff192cda84a889dd
fuczak <prus.damian@gmail.com>
2015-08-13 14:24:45 +0200

    Move sidebar button arrangements to main module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,13 @@
 var $ = window.jQuery = require('jquery');
 require('bootstrap-sass');
 var engine = require('./engine/index');
+var arrangeSidebarButtons = require('./ui/arrangeSidebarButtons');
 
 $(document).ready(function() {
+	// Init match engine
 	engine.init();
+
+	// Arrange sidebar buttons and attach event handler to resize action
+	arrangeSidebarButtons();
+	$(window).resize(arrangeSidebarButtons);
 });
--- src/scripts/ui/init.js
+++ src/scripts/ui/init.js
@@ -2,17 +2,12 @@ var $ = require('jquery');
 var q = require('q');
 var random = require('lodash/number/random');
 var prepareTeams = require('./prepareTeams');
-var arrangeSidebarButtons = require('./arrangeSidebarButtons');
 var setTeams = require('./setTeams');
 var setTactics = require('./setTactics');
 
 module.exports = function init() {
 
 	var deferred = q.defer();
-
-	// Sidebar button group alignment dependant on user device viewport's width
-	arrangeSidebarButtons();
-	$(window).resize(arrangeSidebarButtons);
 
 	// Show available teams
 	$('#overlay').removeClass('overlay-hide');
