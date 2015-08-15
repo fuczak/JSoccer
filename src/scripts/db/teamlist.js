1814628b1f28fa89028da8cf7f41fc71b593969e
Damian Prus <prus.damian@gmail.com>
2015-08-15 13:31:06 +0200

    Add flag prop in team object and modify team node painting function

--- src/scripts/db/teamlist.js
+++ src/scripts/db/teamlist.js
@@ -2,6 +2,7 @@ module.exports = [
   {
     id: 'PL',
     name: 'Poland',
+    flag: 'Poland',
     skill: {
       attack: 100,
       midfield: 100,
@@ -11,6 +12,7 @@ module.exports = [
   {
     id: 'DE',
     name: 'Germany',
+    flag: 'Germany',
     skill: {
       attack: 50,
       midfield: 50,
@@ -20,6 +22,7 @@ module.exports = [
   {
     id: 'BR',
     name: 'Brazil',
+    flag: 'Brazil',
     skill: {
       attack: 50,
       midfield: 50,
@@ -28,7 +31,8 @@ module.exports = [
   },
   {
     id: 'US',
-    name: 'United-States-of-America',
+    name: 'United States',
+    flag: 'United-States-of-America',
     skill: {
       attack: 50,
       midfield: 50,
--- src/scripts/ui/paintTeamNode.js
+++ src/scripts/ui/paintTeamNode.js
@@ -2,7 +2,7 @@ module.exports = function(e) {
 	var node = document.createElement('div');
 	var image = document.createElement('img');
 	var text = document.createTextNode(e.id);
-	image.src = 'images/flags/' + e.name + '.png';
+	image.src = 'images/flags/' + e.flag + '.png';
 	image.classList.add('img-responsive', 'flag-img');
 	node.classList.add('flag', 'text-center', 'col-xs-3', 'col-md-1');
 	node.appendChild(image);
