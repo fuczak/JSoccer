d50d54f76f579f498910c442b8cc9c28b263c545
Damian Prus <prus.damian@gmail.com>
2015-08-17 20:11:35 +0200

    Remove isSuccess prop

--- src/scripts/engine/evaluateOutcome.js
+++ src/scripts/engine/evaluateOutcome.js
@@ -11,52 +11,42 @@ module.exports = function(state, index) {
 
   var shouldContinue = true;
   var isWhistle = false;
-  var isSuccess = false;
 
   switch (picked.outcome.type) {
     case 'Goal':
       shouldContinue = false;
-      isSuccess = true;
       uiCardNumber.decrement(0);
       break;
     case 'Chance':
       shouldContinue = false;
-      isSuccess = true;
       uiCardNumber.decrement(1);
       break;
     case 'Pass':
       shouldContinue = true;
-      isSuccess = true;
       uiCardNumber.decrement(2);
       break;
     case 'Tackle':
       shouldContinue = false;
-      isSuccess = false;
       uiCardNumber.decrement(3);
       break;
     case 'Injury':
       shouldContinue = true;
-      isSuccess = true;
       uiCardNumber.decrement(4);
       break;
     case 'Offside':
       shouldContinue = false;
-      isSuccess = false;
       uiCardNumber.decrement(4);
       break;
     case 'Penalty':
       shouldContinue = false;
-      isSuccess = true;
       uiCardNumber.decrement(4);
       break;
     case 'Red Card':
       shouldContinue = false;
-      isSuccess = false;
       uiCardNumber.decrement(4);
       break;
     case 'Whistle':
       shouldContinue = false;
-      isSuccess = false;
       isWhistle = true;
       uiCardNumber.decrement(4);
       break;
@@ -64,7 +54,6 @@ module.exports = function(state, index) {
 
   return {
     shouldContinue: shouldContinue,
-    isSuccess: isSuccess,
     isWhistle: isWhistle,
     index: picked.index,
     lostEnergy: random(3, 10),
