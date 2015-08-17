1c37bcd9e5dd6beb0d1011a48db1d18c55dd5e91
fuczak <prus.damian@gmail.com>
2015-08-17 13:57:54 +0200

    Remove card flipping animation since it wasn't working very well with ramjet

--- src/scripts/ui/cardToCommentary.js
+++ src/scripts/ui/cardToCommentary.js
@@ -4,7 +4,7 @@ var q = require('q');
 
 module.exports = function(index, type, text) {
 	var deferred = q.defer();
-	var a = document.getElementById(index);
+	var a = $('#' + index).find('.front')[0];
 	var b = document.getElementById('commentary');
 	b.children[0].classList.remove('entered');
 	setTimeout(function() {
@@ -14,14 +14,17 @@ module.exports = function(index, type, text) {
 		  done: function() {
 		   b.children[0].textContent = text;
 		   b.children[0].classList.add('entered');
-		   b.classList.remove('ramjet-hidden');
+		   b.classList.remove('ramjet-hidden');		   
 		   deferred.resolve();
 		  },
 		  duration: 600,
 		  easing: ramjet.easeInOut
 		});
-		$('#' + index).addClass('flipped').find('.back').addClass(type).append('<p class="text-center">' + type + '</p>');
-		// a.classList.add('ramjet-hidden');
+		$('#' + index).find('.back').addClass(type).append('<p class="text-center">' + type + '</p>');
+		$('#' + index).find('.front').addClass('ramjet-hidden');
+		setTimeout(function() {
+			$('#' + index).find('p').addClass('entered');
+		}, 200);
 	}, 150);
 
 	return deferred.promise;
